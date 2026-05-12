cff36adcca72db7a74c1fc60d3bd64764bb4caf2
Tushar Islam <tushar060703@gmail.com>
2026-05-12 11:59:27 -0400

    Part1 completed

--- src/main/java/com/pluralsight/Asset.java
+++ src/main/java/com/pluralsight/Asset.java
@@ -1,6 +1,6 @@
 package com.pluralsight;
 
-public class Asset {
+public abstract class Asset {
     private String description;
     private String dateAcquired;
     private double originalCost;
@@ -27,9 +27,7 @@ public class Asset {
         this.dateAcquired = dateAcquired;
     }
 
-    public double getValue() {
-        return originalCost;
-    }
+    public abstract double getValue();
 
     public void setOriginalCost(double originalCost) {
         this.originalCost = originalCost;
